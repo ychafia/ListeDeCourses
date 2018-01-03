8f94f9cd68d3c19e4e8c2748c1afd7817437fea9
ychafia <chafiayouness@gmail.com>
2018-01-03 12:40:55 +0100

    backend connexion

--- src/main/java/fr/youness/mescourses/dao/UtilisateurImpl.java
+++ src/main/java/fr/youness/mescourses/dao/UtilisateurImpl.java
@@ -1,6 +1,9 @@
 package fr.youness.mescourses.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import fr.youness.mescourses.bean.Utilisateur;
 import fr.youness.mescourses.util.HibernateUtil;
@@ -12,12 +15,20 @@ public class UtilisateurImpl implements IUtilisateur {
 		boolean autoriser = false;
 		Session session = HibernateUtil.getSessionFactory().openSession();	
 		session.beginTransaction();
-        Utilisateur user = (Utilisateur)session.load(Utilisateur.class, new String(u.getIdentifiant()));
-        if(user == null){
-        	autoriser = false;
-        }else{
-        	autoriser = true;
+        Utilisateur user;
+        List users = session.createCriteria(Utilisateur.class).add(Restrictions.like("identifiant", u.getIdentifiant()))
+        		.add(Restrictions.eq("motDePasse", u.getMotDePasse())).list();
+        
+        if(users.size() > 0){
+        	user = (Utilisateur)users.get(0);
+        	if(user.getMotDePasse().equals(u.getMotDePasse())){
+        		autoriser = true;
+        	}
+        	else{
+        		autoriser = false;
+        	}
         }
+        
         session.getTransaction().commit();
         session.clear();
         session.close();
