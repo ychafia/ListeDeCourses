978d6ba34ff8d6135800709ea8153b91c8824d64
ychafia <chafiayouness@gmail.com>
2018-01-03 22:25:45 +0100

    redirect auth

--- src/main/webapp/js/controleurs/authCtrl.js
+++ src/main/webapp/js/controleurs/authCtrl.js
@@ -1,6 +1,6 @@
 
-angular.module('mesCoursesApp').controller('authCtrl', ['$scope', '$http', 'serviceAuth', function($scope, $http, serviceAuth) {
-	console.log("APICourses prête à lancer ...");
+angular.module('mesCoursesApp').controller('authCtrl', ['$scope', '$http', 'serviceAuth', '$location', function($scope, $http, serviceAuth, $location) {
+	console.log("APICourses prï¿½te ï¿½ lancer ...");
 	
 	$scope.seConnecter = function(){
 		//Construction de l'objet ? poster
@@ -9,9 +9,15 @@ angular.module('mesCoursesApp').controller('authCtrl', ['$scope', '$http', 'serv
 			motDePasse : $scope.password
 		};
 		
-		console.log(obj_elt);
+		//console.log(obj_elt);
 		serviceAuth.seConnecter(obj_elt).then(function(response) {
 			$scope.autoriser = response.data;
+			if($scope.autoriser == true){
+				$location.path('/mescourses')
+			}
+			else{
+				$location.path('/')
+			}
 			console.log(response.data);
 		}, function(response) {
 			console.log( "Erreur lors de l'ajout de l'element: " + JSON.stringify({data: response}));
